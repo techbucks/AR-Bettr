be6e9797c0cff7333749e83995d1b6b4d15bfc33
Rizwan Ijaz <rizwanijaz.dev@gmail.com>
2025-05-27 15:15:17 +0500

    Initial clean commit

---
+++ Assets/TestInputs.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+using CesiumForUnity;
+
+public class TestInputs : MonoBehaviour
+{
+    // public TMP_InputField longitudeInput;
+    // public TMP_InputField latitudeInput;
+    // public CesiumGeoreference georeference;
+    
+    // // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+    // public void OnSubmit()
+    // {
+    //     georeference.SetOriginLongitudeLatitudeHeight(double.Parse(longitudeInput.text),double.Parse(latitudeInput.text),1);
+    //     // georeference.Origin.Longitude = float.Parse(longitudeInput.text);
+    //     // georeference.Origin.Latitude = float.Parse(latitudeInput.text);
+
+    //     georeference.ReloadEllipsoids();
+    // }
+
+}
---
+++ Assets/logdebug.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+//#if ARCORE_EXTENSIONS_PRESENT
+using Google.XR.ARCoreExtensions;
+//#endifv
+using Google.XR.ARCoreExtensions.GeospatialCreator;
+
+public class DebugRealWorldLocation : MonoBehaviour
+{
+    private Transform arCamera;
+
+    //#if ARCORE_EXTENSIONS_PRESENT
+    public ARGeospatialCreatorAnchor geoAnchor;
+    //#endif
+
+    void Start()
+    {
+        arCamera = Camera.main.transform;
+
+        //#if ARCORE_EXTENSIONS_PRESENT
+        //      geoAnchor = GetComponent<ARGeospatialAnchor>();
+        if (geoAnchor == null)
+        {
+            Debug.LogWarning("No ARGeospatialAnchor component found on this object.");
+        }
+        //#endif
+    }
+
+    void Update()
+    {
+        // Log Unity world position
+        Debug.Log($"[{gameObject.name}] World Position: {transform.position}");
+
+        // Log relative distance to camera
+        if (arCamera != null)
+        {
+            float distance = Vector3.Distance(transform.position, arCamera.position);
+            Debug.Log($"[{gameObject.name}] Distance to Camera: {distance:F2} units");
+        }
+
+#if ARCORE_EXTENSIONS_PRESENT
+        // Log Geospatial Anchor info if available
+        if (geoAnchor != null)
+        {
+            Debug.Log($"[{gameObject.name}] Geospatial Location: Lat={geoAnchor.Latitude}, Lon={geoAnchor.Longitude}, Alt={geoAnchor.Altitude}");
+        }
+#endif
+    }
+}
